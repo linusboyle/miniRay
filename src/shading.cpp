7dcad739b2bcc72dacb730bd2838cba4d436615c
Zhilei Han <linusboyle@gmail.com>
2019-02-26 12:38:59 +0800

    refactor: allow using stl-like iterator for matrix

--- src/shading.cpp
+++ src/shading.cpp
@@ -4,16 +4,23 @@
 
 namespace graphics {
 
-    RGBColor ShadingPolicy::Lambertian(RGBColor cDiffuse, coordinate_type intensity, Vector3 uNormal, Vector3 lDirection) {
-        return std::max(0.0, scalarProduct(uNormal,normalize(lDirection))) * intensity * cDiffuse;
+    RGBColor ShadingPolicy::Lambertian
+    (RGBColor cDiffuse, coordinate_type intensity, Vector3 uNormal, Vector3 lDirection) {
+        return std::max(0.0, scalarProduct(uNormal,normalize(lDirection)))
+                * intensity
+                * cDiffuse;
     }
 
-    RGBColor ShadingPolicy::BlinnPhong(RGBColor cSpecular, coordinate_type intensity, Vector3 uNormal, Vector3 lDirection, Vector3 vDirection, coordinate_type phongExponent) {
+    RGBColor ShadingPolicy::BlinnPhong
+    (RGBColor cSpecular, coordinate_type intensity, Vector3 uNormal, Vector3 lDirection,
+     Vector3 vDirection, coordinate_type phongExponent) {
         Vector3 ulDirection = normalize(lDirection);
         Vector3 uvDirection = normalize(vDirection);
         Vector3 h = normalize(uvDirection + ulDirection);
 
-        return std::max(0.0, std::pow(scalarProduct(uNormal, h), phongExponent)) * intensity * cSpecular;
+        return std::max(0.0, std::pow(scalarProduct(uNormal, h), phongExponent))
+                * intensity
+                * cSpecular;
     }
 
     RGBColor ShadingPolicy::Ambient(RGBColor cAmbient, coordinate_type aIntensity) {
