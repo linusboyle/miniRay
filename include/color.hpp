9291ec13e099b42c582372299fec3ef24c007607
Zhilei Han <linusboyle@gmail.com>
2019-03-12 23:18:01 +0800

    modify rgbcolor representation a bit

--- include/color.hpp
+++ include/color.hpp
@@ -1,17 +1,29 @@
 #ifndef COLOR_HPP
 #define COLOR_HPP
 
-#include "config.hpp"
-
 namespace graphics {
-    struct RGBColor {
+    class RGBColor {
         double R;
         double G;
         double B;
+    public:
+        RGBColor(int r, int g, int b) : R(r / 255.0), G(g / 255.0), B(b / 255.0) {}
+        RGBColor(double r, double g, double b) : R(r), G(g), B(b) {}
+
+        unsigned char r() const {
+            return static_cast<unsigned char>(R * 255);
+        }
+        unsigned char g() const {
+            return static_cast<unsigned char>(G * 255);
+        }
+        unsigned char b() const {
+            return static_cast<unsigned char>(B * 255);
+        }
+
+        friend RGBColor operator+ (const RGBColor& lhs, const RGBColor& rhs);
+        friend RGBColor operator- (const RGBColor& lhs, const RGBColor& rhs);
+        friend RGBColor operator* (double lhs, const RGBColor& rhs);
     };
 
-    RGBColor operator+ (const RGBColor& lhs, const RGBColor& rhs);
-    RGBColor operator- (const RGBColor& lhs, const RGBColor& rhs);
-    RGBColor operator* (double lhs, const RGBColor& rhs);
 }
 #endif /* ifndef COLOR_HPP */
