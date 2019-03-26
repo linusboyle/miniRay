3c6378ce36a1c99ffd4a7d295caf46d8e9bf6ec7
Zhilei Han <linusboyle@gmail.com>
2019-03-26 12:37:50 +0800

    disable raster test for now

--- test/image_test.cpp
+++ test/image_test.cpp
@@ -15,10 +15,10 @@ TEST_CASE ("image can be manipulated correctly", "[image]") {
     REQUIRE(img.height() == 512);
     REQUIRE(img.width() == 512);
 
-    SECTION("Image can fill the right color") {
-        img.fillcolor({12, 109, 71});
-        img.show();
-    }
+    //SECTION("Image can fill the right color") {
+        //img.fillcolor({12, 109, 71});
+        //img.show();
+    //}
 
     SECTION("Image can create real image file correctly") {
         fs::path rendered_path_png = "./blackbg.png";
@@ -36,103 +36,103 @@ TEST_CASE ("image can be manipulated correctly", "[image]") {
         REQUIRE(fs::exists(rendered_path_ppm));
     }
 
-    SECTION("Image draws line correctly in all directions") {
-        img.fillcolor({255, 255, 255});
-        img.drawline(200, 200, 200, 100, {0, 0, 0});
-        img.drawline(200, 200, 500, 450, {0, 0, 0});
-        img.drawline(200, 200, 500, 10, {0, 0, 0});
-        img.drawline(200, 200, 10, 500, {0, 0, 0});
-        img.drawline(200, 200, 30, 10, {0, 0, 0});
+    //SECTION("Image draws line correctly in all directions") {
+        //img.fillcolor({255, 255, 255});
+        //img.drawline(200, 200, 200, 100, {0, 0, 0});
+        //img.drawline(200, 200, 500, 450, {0, 0, 0});
+        //img.drawline(200, 200, 500, 10, {0, 0, 0});
+        //img.drawline(200, 200, 10, 500, {0, 0, 0});
+        //img.drawline(200, 200, 30, 10, {0, 0, 0});
 
-        img.show();
-    }
+        //img.show();
+    //}
 
-    SECTION("draw circle correctly") {
-        img.fillcolor({255, 255, 255});
-        img.drawcircle(200, 200, 100, {0, 0, 0});
-        img.show();
-    }
+    //SECTION("draw circle correctly") {
+        //img.fillcolor({255, 255, 255});
+        //img.drawcircle(200, 200, 100, {0, 0, 0});
+        //img.show();
+    //}
 
-    SECTION("draw circle with anti aliasing correctly") {
-        img.fillcolor({255, 255, 255});
-        img.setAntialiasing();
+    //SECTION("draw circle with anti aliasing correctly") {
+        //img.fillcolor({255, 255, 255});
+        //img.setAntialiasing();
 
-        img.drawcircle(100, 100, 70, {0, 0, 0});
-        img.drawcircle(240, 110, 50, {120, 90, 114});
-        img.drawcircle(150, 210, 100, {240, 100, 78});
-        img.drawcircle(300, 210, 200, {200, 150, 10});
+        //img.drawcircle(100, 100, 70, {0, 0, 0});
+        //img.drawcircle(240, 110, 50, {120, 90, 114});
+        //img.drawcircle(150, 210, 100, {240, 100, 78});
+        //img.drawcircle(300, 210, 200, {200, 150, 10});
 
-        img.show();
-    }
+        //img.show();
+    //}
 
-    SECTION("Anti aliasing line drawing in all directions") {
-        img.fillcolor({255, 255, 255});
-        img.setAntialiasing();
+    //SECTION("Anti aliasing line drawing in all directions") {
+        //img.fillcolor({255, 255, 255});
+        //img.setAntialiasing();
 
-        img.drawline(200, 200, 500, 450, {0, 0, 0});
-        img.drawline(200, 200, 500, 10, {0, 0, 0});
-        img.drawline(200, 200, 10, 500, {0, 0, 0});
-        img.drawline(200, 200, 30, 10, {0, 0, 0});
+        //img.drawline(200, 200, 500, 450, {0, 0, 0});
+        //img.drawline(200, 200, 500, 10, {0, 0, 0});
+        //img.drawline(200, 200, 10, 500, {0, 0, 0});
+        //img.drawline(200, 200, 30, 10, {0, 0, 0});
 
-        img.show();
-    }
+        //img.show();
+    //}
 
-    SECTION("Anti aliasing line drawing is correct with intersection") {
-        img.fillcolor({255, 255, 255});
-        img.setAntialiasing();
+    //SECTION("Anti aliasing line drawing is correct with intersection") {
+        //img.fillcolor({255, 255, 255});
+        //img.setAntialiasing();
 
-        // a triangle
-        img.drawline(90, 230, 400, 390, {0, 0, 0});
-        img.drawline(20, 300, 400, 39, {0, 0, 0});
-        img.drawline(300, 20, 370, 400, {0, 0, 0});
+        //// a triangle
+        //img.drawline(90, 230, 400, 390, {0, 0, 0});
+        //img.drawline(20, 300, 400, 39, {0, 0, 0});
+        //img.drawline(300, 20, 370, 400, {0, 0, 0});
 
-        img.show();
-    }
+        //img.show();
+    //}
 
-    SECTION("polygon drawn correctly") {
-        img.fillcolor({255, 255, 255});
-        img.setAntialiasing();
+    //SECTION("polygon drawn correctly") {
+        //img.fillcolor({255, 255, 255});
+        //img.setAntialiasing();
 
-        raster::PointList points;
-        points << Point2dR{30, 10} << Point2dR{20, 300} << Point2dR{400, 300}
-               << Point2dR{390, 10} << Point2dR{210, 200};
+        //raster::PointList points;
+        //points << Point2dR{30, 10} << Point2dR{20, 300} << Point2dR{400, 300}
+               //<< Point2dR{390, 10} << Point2dR{210, 200};
 
-        img.drawpolygon(points, {0, 0, 0});
+        //img.drawpolygon(points, {0, 0, 0});
 
 
-        img.show();
-    }
+        //img.show();
+    //}
 }
 
-TEST_CASE("image fill correctly", "[image]") {
-    Image img(800, 800);
-    img.fillcolor({0, 0, 0});
+//TEST_CASE("image fill correctly", "[image]") {
+    //Image img(800, 800);
+    //img.fillcolor({0, 0, 0});
 
-    raster::PointList points;
-    points << Point2dR{400, 200} << Point2dR{227, 500} << Point2dR{573, 500};
+    //raster::PointList points;
+    //points << Point2dR{400, 200} << Point2dR{227, 500} << Point2dR{573, 500};
 
-    img.drawpolygon(points, {200, 200, 100});
-    img.fillcolor(400, 400, {200, 200, 100});
-    img.filter();
+    //img.drawpolygon(points, {200, 200, 100});
+    //img.fillcolor(400, 400, {200, 200, 100});
+    //img.filter();
 
-    img.show();
-}
+    //img.show();
+//}
 
-TEST_CASE("put it all together", "image") {
-    Image img(800, 800);
-    img.setAntialiasing();
-    img.fillcolor({255, 255, 255});
+//TEST_CASE("put it all together", "image") {
+    //Image img(800, 800);
+    //img.setAntialiasing();
+    //img.fillcolor({255, 255, 255});
 
-    img.drawcircle(400, 400, 200, {244, 208, 63});
-    img.drawcircle(330, 330, 50, {244, 208, 63});
-    img.drawcircle(470, 330, 50, {244, 208, 63});
+    //img.drawcircle(400, 400, 200, {244, 208, 63});
+    //img.drawcircle(330, 330, 50, {244, 208, 63});
+    //img.drawcircle(470, 330, 50, {244, 208, 63});
 
-    raster::PointList points;
-    points << Point2dR{400, 400} << Point2dR{357, 475} << Point2dR{443, 475};
+    //raster::PointList points;
+    //points << Point2dR{400, 400} << Point2dR{357, 475} << Point2dR{443, 475};
 
-    img.drawpolygon(points, {244, 208, 63});
+    //img.drawpolygon(points, {244, 208, 63});
 
-    img.filter();
+    //img.filter();
 
-    img.show();
-}
+    //img.show();
+//}
