1a1c57bd4b9430682cc11025f385aa225dd2f678
Zhilei Han <linusboyle@gmail.com>
2019-04-17 19:23:38 +0800

    use vector3 in rgbcolor

--- src/color.cpp
+++ src/color.cpp
@@ -1,44 +1,21 @@
 #include "color.hpp"
 
 namespace graphics {
-RGBColor operator+(const RGBColor& lhs, const RGBColor& rhs) {
-  double r_ = lhs.R + rhs.R;
-  if (r_ > 1.0) r_ = 1.0;
-  double g_ = lhs.G + rhs.G;
-  if (g_ > 1.0) g_ = 1.0;
-  double b_ = lhs.B + rhs.B;
-  if (b_ > 1.0) b_ = 1.0;
-
-  return {r_, g_, b_};
+RGBColor operator+(const RGBColor &lhs, const RGBColor &rhs) {
+  return {lhs.rgb_ + rhs.rgb_};
 }
 
-RGBColor operator-(const RGBColor& lhs, const RGBColor& rhs) {
-  double r_ = lhs.R - rhs.R;
-  if (r_ < 0.0) r_ = 0.0;
-  double g_ = lhs.G - rhs.G;
-  if (g_ < 0.0) g_ = 0.0;
-  double b_ = lhs.B - rhs.B;
-  if (b_ < 0.0) b_ = 0.0;
-
-  return {r_, g_, b_};
+RGBColor operator-(const RGBColor &lhs, const RGBColor &rhs) {
+  return {lhs.rgb_ - rhs.rgb_};
 }
 
-RGBColor operator*(double lhs, const RGBColor& rhs) {
-  double r_ = lhs * rhs.R;
-  if (r_ > 1.0) r_ = 1.0;
-  double g_ = lhs * rhs.G;
-  if (g_ > 1.0) g_ = 1.0;
-  double b_ = lhs * rhs.B;
-  if (b_ > 1.0) b_ = 1.0;
+RGBColor operator*(double lhs, const RGBColor &rhs) { return {lhs * rhs.rgb_}; }
 
-  return {r_, g_, b_};
-}
-
-bool operator==(const RGBColor& lhs, const RGBColor& rhs) {
+bool operator==(const RGBColor &lhs, const RGBColor &rhs) {
   // almost equal
   if (lhs.r() == rhs.r() && lhs.b() == rhs.b() && lhs.g() == rhs.g())
     return true;
   return false;
 }
 
-}  // namespace graphics
+} // namespace graphics
